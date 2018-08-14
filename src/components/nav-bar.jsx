a48c93acb1a3ba62913bb1d086898db9288a678c
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-08-14 15:16:58 +0100

    initial file set up

---
+++ src/components/nav-bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/nav-bar.css';
+
+const NavBar = () => (
+  <div className="NavBar">
+    NavBar
+    <ul>
+      <li><i className="fas fa-building"></i></li>
+      <li>buttons</li>
+      <li>buttons</li>
+    </ul>
+  </div>
+);
+
+export default NavBar;
